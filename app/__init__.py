c9b7249b7ed1945d263eafb113247c939ef65c13
Fadipe Daniel <fadipemonioluwa@gmail.com>
2022-11-03 23:38:12 +0100

    Add login_manager and UserMixin to manage sessions

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -10,5 +11,6 @@ app.config['SECRET_KEY'] = '9892b398j38989bhi7b30gg332'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from app import routes
--- app/models.py
+++ app/models.py
@@ -1,8 +1,12 @@
 from datetime import datetime
-from app import db
+from app import db, login_manager
+from flask_login import UserMixin
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(20), unique=True, nullable=False)
     last_name = db.Column(db.String(20), unique=True, nullable=False)
@@ -15,7 +19,7 @@ class User(db.Model):
         return f"User('{self.first_name}', '{self.email}', '{self.image_file}')"
 
 
-class Article(db.Model):
+class Article(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
