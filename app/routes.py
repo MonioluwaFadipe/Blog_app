3e91659b2b6cfdb0593f7a91ad3848537e709250
Fadipe Daniel <fadipemonioluwa@gmail.com>
2022-11-04 06:35:52 +0100

    Add login validation

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from app import app, db, bcrypt
 from app.forms import LoginForm, RegistrationForm
 from app.models import User, Article
+from flask_login import login_user
 
 
 posts = [
@@ -32,8 +33,9 @@ def about():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash("You have been logged in", "success")
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
             return redirect(url_for('home'))
         else:
             flash('Unsucessful attempt. Please check your email or password', 'danger')
